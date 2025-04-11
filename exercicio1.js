599cfaaab73fa965fc129b6e43fa0e30fa10bd79
mays-a <o.maysa@aluno.ifsp.edu.br>
2025-04-11 15:59:12 -0300

    Update exercicio1.js

--- exercicio1.js
+++ exercicio1.js
@@ -1,1 +1,4 @@
-
+function soma(a,b){ //'soma': é nome da função; '(a,b)': são os parâmetros
+  return a+b //'return': é o retorno da função
+}
+console.log(soma(25,36)) //'(25,36)': são os números que irão substituir os parâmetros na função
